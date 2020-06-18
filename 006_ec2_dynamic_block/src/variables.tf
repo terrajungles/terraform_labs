3b0b24830a0b9427669e16c9ce6bd3fc336e1f30
Thiam Hock Ng <nthock@gmail.com>
2020-06-18 17:27:43 +0800

    Include dynamic block

---
+++ 006_ec2_dynamic_block/src/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  type = string
+}
+
+variable "greeting_message" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "ingress_ports" {
+  type = list(number)
+  description = "List of ingress ports"
+  default     = [80, 443]
+}
