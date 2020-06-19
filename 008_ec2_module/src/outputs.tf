21e25e00ada25fadaed4e8976a919c1eb8765936
Thiam Hock Ng <nthock@gmail.com>
2020-06-19 19:46:22 +0800

    remove unnecessary code

--- 008_ec2_module/src/outputs.tf
+++ 008_ec2_module/src/outputs.tf
@@ -3,16 +3,6 @@ output "ami_id" {
   value       = data.aws_ami.amazon_linux_2.id
 }
 
-# output "instance_public_ip" {
-#   value       = aws_instance.sample[*].public_ip
-#   description = "Public IP of the AWS instance"
-# }
-
-# output "instance_public_dns" {
-#   value       = aws_instance.sample[*].public_dns
-#   description = "Public DNS of the AWS instance"
-# }
-
 output "instance_public_ip" {
   value       = module.aws_compute_instances.instance_public_ip
   description = "Public IP of the AWS instance"
